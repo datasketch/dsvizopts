1e5fd39a69180711ed5aa2e2342965ca51193a6e
Camila Achury <camilaachury.4@gmail.com>
2020-07-23 15:51:35 -0500

    remove comma

--- R/default_styles.R
+++ R/default_styles.R
@@ -3,7 +3,7 @@ datasketch_style <- function(){
   list(
     palette = c("#385573", "#ffa92a", "#f06142",
                 "#99e8b3", "#32a8ce", "#996295",
-                "#e59fd7",),
+                "#e59fd7"),
     background = "#fafafa",
     na = "#d1d9db"
   )
