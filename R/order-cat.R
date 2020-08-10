f74468b22b29eb438dfb20dbfd657e56dca1cf11
Camila Achury <camilaachury.4@gmail.com>
2020-08-10 10:46:17 -0300

    new param to add line

--- R/order-cat.R
+++ R/order-cat.R
@@ -1,7 +1,7 @@
 
 # order category column
 #'@export
-order_category <- function(data, col, order, label_wrap) {
+order_category <- function(data, col, order, label_wrap, new_line = "<br/>") {
   if(!class(data[[col]]) %in% c("character", "factor")) {
     warning("Category column must be character or factor")
     return(data)
@@ -16,7 +16,7 @@ order_category <- function(data, col, order, label_wrap) {
   if (is.null(label_wrap)) {
     data <- data
   } else {
-    data[[col]] <- gsub("\\\n", "<br/>", str_wrap(data[[col]], label_wrap))
+    data[[col]] <- gsub("\\\n", new_line, str_wrap(data[[col]], label_wrap))
   }
   data
 }
