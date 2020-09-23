c989659eb37a3a37b92ae9ea578404a4af258939
Lena Mangold <lena@mangold.org>
2020-09-23 08:55:43 -0500

    add sequential and divergent palettes to default styles

--- R/default_styles.R
+++ R/default_styles.R
@@ -4,8 +4,13 @@ datasketch_style <- function(){
     palette = c("#385573", "#ffa92a", "#f06142",
                 "#99e8b3", "#32a8ce", "#996295",
                 "#e59fd7"),
+    sequential = c("#ff91ff", "#f082f8", "#dd77f1",
+                   "#c66eeb", "#a968e5", "#8465e0",
+                   "#4f63db"),
+    divergent = c('#6f56fa', '#ad87fc', '#d6bdfb',
+                  '#f7f5f7', '#ffc9aa', '#ff9791',
+                  '#fc4eb9'),
     background = "#fafafa",
     na = "#d1d9db"
   )
 }
-
