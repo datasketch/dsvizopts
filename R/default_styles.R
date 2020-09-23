40371b0691d9abece0313cd295839009a2d3d01d
Lena Mangold <lena@mangold.org>
2020-09-23 09:21:33 -0500

    update default colors

--- R/default_styles.R
+++ R/default_styles.R
@@ -4,12 +4,12 @@ datasketch_style <- function(){
     palette = c("#385573", "#ffa92a", "#f06142",
                 "#99e8b3", "#32a8ce", "#996295",
                 "#e59fd7"),
-    sequential = c("#ff91ff", "#f082f8", "#dd77f1",
-                   "#c66eeb", "#a968e5", "#8465e0",
-                   "#4f63db"),
-    divergent = c('#6f56fa', '#ad87fc', '#d6bdfb',
-                  '#f7f5f7', '#ffc9aa', '#ff9791',
-                  '#fc4eb9'),
+    sequential = c("#b8f3ca", "#add79c", "#8dbd93",
+                   "#70a38e", "#568989", "#3f6f83",
+                   "#28557d"),
+    divergent = c("#ff844e", "#ffa154", "#ffc48b",
+                  "#eef0f1", "#a0d7ca", "#57a0b5",
+                  "#255b89"),
     background = "#fafafa",
     na = "#d1d9db"
   )
