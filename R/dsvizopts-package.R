9ddbc508ed23729d60e4934565d5d8c7baf0bbb8
Camila Achury <camilaachury.4@gmail.com>
2021-02-09 15:01:18 -0300

    theme options

--- R/dsvizopts-package.R
+++ R/dsvizopts-package.R
@@ -28,3 +28,65 @@ NULL
 #' @keywords internal
 #' @name dsvizopts_palette_type
 NULL
+
+#' Theme options
+#' @description
+#'
+#' @param logo Logo path or url
+#' @param logo_position Logo "left" or "right"
+#' @param palette_colors Vector of colors
+#' @param palette_colors_categorical Vector of colors suitable for categorical variable
+#' @param palette_colors_sequential Vector of sequential colors suitable for numerical variable
+#' @param palette_colors_divergent Vector of divergent colors suitable for numerical variable
+#' @param palette_type Type of color palette "categorical", "sequential", or "divergent"
+#' @param branding_include Include branding logical
+#' @param branding_text Text for branding bar
+#' @param background_color Plot background color
+#' @param branding_background_color Branding bar background color
+#' @param accent_color ?
+#' @param text_show ?
+#' @param text_size Size of text in pt
+#' @param text_color Text color
+#' @param text_family Font
+#' @param line_color Axis and grid line colors
+#' @param line_size Line size
+#' @param title_color Color for title text
+#' @param title_align Alignment of title "left", "center" or "right"
+#' @param title_family Title font
+#' @param title_size Title size, defaults to 1.1 text_size
+#' @param title_weight Font weight for title
+#' @param subtitle_color Color for subtitle text
+#' @param subtitle_align Alignment of subtitle "left", "center" or "right"
+#' @param subtitle_size Subtitle size, defaults to 1.1 text_size
+#' @param subtitle_weight Font weight for subtitle
+#' @param caption_color Color for caption text
+#' @param caption_align Alignment of caption "left", "center" or "right"
+#' @param caption_size caption size, defaults to 1.1 text_size
+#' @param axis_title_color Color for axis text
+#' @param axis_title_size Axis text size
+#' @param axis_line_color Axis line color
+#' @param axis_line_size Axis line width
+#' @param axis_ticks_color Axis ticks color
+#' @param axis_tick_length Axis tick length
+#' @param grid_x_enabled Enable horizontal grid
+#' @param grid_x_color Horizontal grid color
+#' @param grid_x_size Horizontal grid line width
+#' @param grid_y_enabled Enable vertical grid
+#' @param grid_y_color Vertical grid color
+#' @param grid_y_size Verical grid line width
+#' @param grid_y_dash ?
+#' @param plot_margin_bottom Plot margin bottom
+#' @param plot_margin_top Plot margin top
+#' @param plot_margin_left Plot margin left
+#' @param plot_margin_right Plot margin right
+#' @param plot_background_color ? Chart background color
+#' @param plot_border_color ? Chart border color
+#' @param plot_border_size ? Chart border width
+#' @param label_color ? Text color
+#' @param label_size ? Text size
+#' @param label_text_decoration ? Text decoration
+#' @param label_text_outline ? Text outline
+#' @keywords internal
+#' @name dsvizopts_theme
+NULL
+
