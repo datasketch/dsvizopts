596ace83ec313f8a16aa9cb7e32ad2cb583fabda
Camila Achury <camilaachury.4@gmail.com>
2021-02-18 22:12:48 -0300

    add tiles maps options

--- R/theme.R
+++ R/theme.R
@@ -174,7 +174,11 @@ default_theme_opts <- function( logo = NULL,
     border_weight = 2,
     border_color = "#28333f",
     topo_fill_opacity = 0.8,
+    map_provider_tile = "leaflet", #leaflet or ESRI
+    map_extra_layout = NULL, #other layouts (URL)
+    map_name_layout = NULL,
     map_tiles = NULL,
+    map_tiles_esri = "Topographic",
     map_zoom = TRUE,
 
     # Lines and areas
