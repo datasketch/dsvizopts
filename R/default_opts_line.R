6e43bd1416b1d0165fb6be676b3a282cd945e54c
CamilaAchuri <camilaachury.4@gmail.com>
2023-03-13 18:27:07 -0300

    special double axis names in lines

--- R/default_opts_line.R
+++ R/default_opts_line.R
@@ -2,7 +2,9 @@
 
 default_opts_line <- function(){
   list(
-    line_spline = FALSE
+    line_spline = FALSE,
+    line_double_axis_left = NULL,
+    line_double_axis_right = NULL
     #date_intervals = NULL,
   )
 }
