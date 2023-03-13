164f50214192a355e7c55d78ad1a142243d2c9ca
CamilaAchuri <camilaachury.4@gmail.com>
2023-03-13 18:45:54 -0300

    change name of line options

--- R/default_opts_line.R
+++ R/default_opts_line.R
@@ -3,8 +3,8 @@
 default_opts_line <- function(){
   list(
     line_spline = FALSE,
-    line_double_axis_left = NULL,
-    line_double_axis_right = NULL
+    line_double_axis_title_left = NULL,
+    line_double_axis_title_right = NULL
     #date_intervals = NULL,
   )
 }
